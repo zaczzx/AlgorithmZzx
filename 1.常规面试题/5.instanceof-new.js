edff93bca322d3d0f84d6e197b72be55ba3b7ff9
zhouzaoxuan959 <zhouzaoxuan959@hellobike.com>
2022-01-21 13:56:51 +0800

    feat: finish instanceof and new

--- 1.常规面试题/5.instanceof-new.js
+++ 1.常规面试题/5.instanceof-new.js
@@ -1,28 +1,34 @@
-// instanceof
-function myInstanceof(left, right) {
-  left = left.__proto__;
-  while(true){
-    if (left === null) return false;
-    if (left === right.prototype) return true;
-    left = left.__proto__;
+/**
+ * instanceof, 循环原型链判断
+ * 参数: 1. 被判断的对象 2. 判断的目标对象
+ * 返回值: boolean, true: 是目标对象的实力, false: 不是目标对象的实例
+ * __proto__: 普通对象都有一个__proto__, 指向 prototype
+ * prototype: 函数对象都有一个 prototype 属性, 指向自己的原型对象
+ */
+function myInstanceof(obj, target){
+  if (!obj || typeof obj != 'object') return false;
+  while(obj.__proto__ != null) {
+    if (obj.__proto__ === target.prototype) return true;
+    obj = obj.__proto__;
   }
+  return false;
 }
 
 console.log(myInstanceof([], Array))
 console.log([] instanceof Array);
 
-// 优化后 new 实现
-function myNew() {
-  // 1、获得构造函数，同时删除 arguments 中第一个参数
-  let Con = [].shift.call(arguments);
-  console.log('Con', Con);
-  // 2、创建一个空的对象并链接到原型，obj 可以访问构造函数原型中的属性
-  let obj = Object.create(Con.prototype);
-  // 3、绑定 this 实现继承，obj 可以访问到构造函数中的属性
-  let ret = Con.apply(obj, arguments);
-  // 4、优先返回构造函数返回的对象
+/**
+ * new, 通过构造函数创建一个实例对象, 实例对象的原型指向构造函数的原型, 如果构造函数有返回值, 返回, 否则返回实例对象
+ * 参数: 无
+ * 返回值: 如果构造函数有返回值, 返回, 否则返回实例对象
+ */
+function myNew(fn, ...args) {
+  // 1、创建一个空的对象并链接到原型，obj 可以访问构造函数原型中的属性
+  let obj = Object.create(fn.prototype);
+  // 2、绑定 this 实现继承，obj 可以访问到构造函数中的属性
+  let ret = fn.apply(obj, args);
+  // 3、优先返回构造函数返回的对象
   return ret instanceof Object ? ret : obj;
-  // 或者 return typeof res === 'object' ? res : obj;
 };
 
 function dog(name, sex){
@@ -43,10 +49,3 @@ console.log(new dog('d', 'male'));
 console.log(myNew(dog, 'd', 'male'));
 console.log(new cat('c', 'female'));
 console.log(myNew(cat, 'c', 'female'));
-
-// Object.create
-Object.myCreate = function(proto){
-  const f = function (){}
-  f.prototype = proto;
-  return new f();
-}
